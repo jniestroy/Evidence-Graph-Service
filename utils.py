ce8249278bda5dff9b3bc69743870580b3bcc8e6
jniestroy <jniestroy@gmail.com>
2020-04-24 10:47:27 -0400

    ORS call functions

--- utils.py
+++ utils.py
@@ -5,12 +5,46 @@ SD_URL = os.environ.get('STARDOG_URL','http://stardog.uvadcos.io')
 SD_USERNAME = os.environ.get('STARDOG_USERNAME')
 SD_PASSWORD = os.environ.get('STARDOG_PASSWORD')
 
+ORS_URL = os.environ.get("ORS_URL","ors.uvadco.io/")
+
 conn_details = {
         'endpoint': SD_URL,
         'username': SD_USERNAME,
         'password': SD_PASSWORD
     }
 conn = stardog.Connection('db', **conn_details)
+
+def mint_eg_id(eg):
+
+    r = requests.post(ORS_URL + 'shoulder/99999')
+
+def add_eg_to_og_id(ark,eg_id):
+
+    r = requests.put(ORS_URL + ark,
+                data=json.dumps({'eg:hasEvidenceGraph':eg_id}))
+
+def eg_exists(ark):
+
+    r = requests.get(ORS_URL + ark)
+
+    meta = r.json()
+
+    if 'eg:hasEvidenceGraph' in meta.keys():
+        return True, meta['eg:hasEvidenceGraph']
+    elif 'error' in meta.keys():
+        raise Exception
+
+    return False,0
+
+def existing_eg(eg_id):
+
+    r = requests.get(ORS_URL + eg_id)
+    eg = r.json()
+    return eg
+
+
+
+
 
 def query_stardog(ark):
 
@@ -22,7 +56,9 @@ def query_stardog(ark):
     return df_eg
 
 def build_evidence_graph(data,clean = True):
+
     eg = {}
+
     context = {'http://www.w3.org/1999/02/22-rdf-syntax-ns#':'@',
           'http://schema.org/':'',
            'http://example.org/':'eg:',
@@ -31,13 +67,14 @@ def build_evidence_graph(data,clean = True):
     trail = []
 
     for index, row in data.iterrows():
+
         if pd.isna(row['x']):
             trail = []
             continue
 
         if clean:
-            for key in context:
 
+            for key in context:
                 if key in row['p']:
                     row['p'] = row['p'].replace(key,context[key])
                 if key in row['y']:
@@ -47,25 +84,32 @@ def build_evidence_graph(data,clean = True):
             eg['@id'] = row['x']
 
         if trail == []:
+
             if row['p'] not in eg.keys():
                 eg[row['p']] = row['y']
+
             else:
                 trail.append(row['p'])
                 if not isinstance(eg[row['p']],dict):
                     eg[row['p']] = {'@id':row['y']}
 
             continue
+
         current = eg
         for t in trail:
             current = current[t]
+
         if not isinstance(current,dict):
             continue
+
         if row['p'] not in current.keys():
-                current[row['p']] = row['y']
+            current[row['p']] = row['y']
         else:
             trail.append(row['p'])
+
             if not isinstance(current[row['p']],dict):
                 current[row['p']] = {'@id':row['y']}
+
     return eg
 
 
