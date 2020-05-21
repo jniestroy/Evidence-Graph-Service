7ceda06ba22fbd6e4cbbdc222618643a064aab5e
jniestroy <jniestroy@gmail.com>
2020-05-21 10:31:31 -0400

    Improved logging + comments

--- evidence-graph.py
+++ evidence-graph.py
@@ -1,40 +1,51 @@
 import flask, stardog, logging
 import pandas as pd
 from flask import Flask, render_template, request, redirect,jsonify
+from utils import *
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-@app.route('/<ark>')
-def homepage(ark):
+app.url_map.converters['everything'] = EverythingConverter
+
+@app.route('/')
+def homepage():
+    return 'working'
+
+@app.route('/eg/<everything:ark>')
+def eg_builder(ark):
 
     logger.info('Homepage handling request %s', request)
 
+    #Check to make sure request is for known ark
     try:
         exists, eg_id = eg_exists(ark)
     except:
-        logger.error('Ark does not exists')
+        logger.error('User gievn ark does not exist ' + str(ark))
         return jsonify({'error':'Given ark does not exist.'}),503
 
-    if exists:
-        logger.info('Request for existing evidence graph: %s', ark)
-        return existing_eg(eg_id)
 
     logger.info('Creating Evidence Graph for %s', ark)
-
     try:
         eg = create_eg(ark)
     except:
-        logger.error('Failed to create eg for ark: %s',ark)
-        return jsonify('error':'Server failed to create evidence graph.'),503
+        logger.error('Failed to create eg for ark: %s',ark,
+                        exc_info=True)
+        return jsonify({'error':'Server failed to create evidence graph.'}),503
 
+    #
+    # Mint ark for evidence graph
+    #
     try:
         eg_id = mint_eg_id(eg)
-        add_eg_to_og_id(ark,eg)
+        add_eg_to_og_id(ark,eg_id)
     except:
-        logger.error('Minting evidence graph failed.')
+        logger.error('Minting evidence graph failed.',exc_info=True)
         return eg
-        
+
     return eg
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
-import os, io, stardog
+import os, io, stardog,json
 import pandas as pd
+from werkzeug.routing import PathConverter
+import requests
+
+#session = requests.Session()
+#session.auth = (SD_USERNAME,SD_PASSWORD)
 
 SD_URL = os.environ.get('STARDOG_URL','http://stardog.uvadcos.io')
 SD_USERNAME = os.environ.get('STARDOG_USERNAME')
@@ -12,18 +17,30 @@ conn_details = {
         'username': SD_USERNAME,
         'password': SD_PASSWORD
     }
-conn = stardog.Connection('db', **conn_details)
+conn = stardog.Connection('ors', **conn_details)
+
+class EverythingConverter(PathConverter):
+    regex = '.*?'
 
 def mint_eg_id(eg):
+    '''
+    Mints Id for newly created evidence graph
+    '''
 
-    r = requests.post(ORS_URL + 'shoulder/99999')
+    r = requests.post(ORS_URL + "shoulder/ark:99999",data = json.dumps(eg))
+
+    if 'created' in r.json():
+        return r.json()['created']
 
 def add_eg_to_og_id(ark,eg_id):
 
-    r = requests.put(ORS_URL + ark,
+    r = requests.put(ORS_URL +  ark,
                 data=json.dumps({'eg:hasEvidenceGraph':eg_id}))
 
 def eg_exists(ark):
+    '''
+    Pings ors to see if ark is reconginzed
+    '''
 
     r = requests.get(ORS_URL + ark)
 
@@ -43,20 +60,24 @@ def existing_eg(eg_id):
     return eg
 
 
-
-
-
 def query_stardog(ark):
+    '''
+    Performs the path query
+    '''
 
     results = conn.paths("PATHS START ?x=<"+ ark + "> END ?y VIA ?p",content_type='text/csv')
     string_csv = io.StringIO(results.decode("utf-8"))
 
-    df_eg = pd.read_csv(TESTDATA, sep=",")
+    df_eg = pd.read_csv(string_csv, sep=",")
 
     return df_eg
 
-def build_evidence_graph(data,clean = True):
 
+def build_evidence_graph(data,clean = True):
+    '''
+    Parses the ugly csv returned from stardog
+    to build json evidence graph
+    '''
     eg = {}
 
     context = {'http://www.w3.org/1999/02/22-rdf-syntax-ns#':'@',
@@ -114,25 +135,36 @@ def build_evidence_graph(data,clean = True):
 
 
 def clean_eg(eg,eg_only = True):
+    '''
+    Goes through json evidence graph removes keys unrelated to basic in or
+    the evi ontology fixes minor formatting things
+    '''
 
     for key in list(eg):
 
-        if 'eg' not in key and key != '@id' and key != 'author' and key != 'name':
+        if 'eg' not in key and key != '@id' and key != 'author' and key != 'name' and key != '@type':
             eg.pop(key, None)
             continue
-        if isinstance(eg[key],dict):
 
+        if isinstance(eg[key],dict):
             if len(eg[key]) == 1:
                 #if dict only has one make it no longer a dict
                 #just value instead
                 eg[key] = eg[key][list(eg[key].keys())[0]]
-
             else:
 
                 eg[key] = clean_eg(eg[key])
+
     return eg
 
 def create_eg(ark):
+    '''
+    Runs all functions required to create an evidence graph
+    ie
+        1.) Path query
+        2.) Parse from csv -> json
+        3.) Clean up json
+    '''
 
     df_eg = query_stardog(ark)
     eg = build_evidence_graph(df_eg)
